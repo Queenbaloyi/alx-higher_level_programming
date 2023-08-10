5a12795c3d478dd7daf1c8dd39a30ee17d9990a7
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 15:56:50 -0700

    update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # 7-islower.py
 
+
 def islower(c):
     """Check for lowercase characters."""
-    lowercase_alphabet = {
-        chr(i): True for i in range(97, 123)
-    }
-    return c in lowercase_alphabet
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
