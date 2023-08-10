a82f9d3f1aa002cdd4afd28bd76912ca1fdbadf2
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 15:10:03 -0700

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def print_alphabet_without_q_and_e():
-  """This function prints the alphabet without the letters 'q' and 'e'."""
-  for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-      print(chr(letter), end="")
+for alphabet_letter in range(97, 123):
+    if chr(alphabet_letter) is not 'q' and chr(alphabet_letter) is not 'e':
+        print("{}".format(chr(alphabet_letter)), end="")
