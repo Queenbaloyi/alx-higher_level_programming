6cda58463e0a571b346ad99423ec31f5ffc2046a
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 16:13:31 -0700

    update

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 # 10-add.py
 
-def sum_two_numbers(first_number, second_number):
-    """Return the sum of two numbers."""
-    return first_number + second_number
 
-
-if __name__ == "__main__":
-    print(sum_two_numbers(1, 2))
+def add(first_number, second_number):
+    """Return the addition of a and b."""
+    return (first_number + second_number)
