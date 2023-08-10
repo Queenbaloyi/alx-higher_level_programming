bf2113d2066d53e7677618cb25307d91ba882972
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-09 17:49:41 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 my_string = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-my_string = my_string[39:74] + my_string[105:112] + my_string[:6]
+my_string = my_string[39:67] + my_string[107:112] + my_string[:6]
 print(my_string)
