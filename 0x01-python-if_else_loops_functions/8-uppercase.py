c88d67c40ff1e515386c9514d774eea6c97b74c8
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 16:04:43 -0700

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,11 @@
-def uppercase(str):
-    """Print a string in uppercase followed by a new line."""
-    for c in str:
+#!/usr/bin/python3
+# 8-uppercase.py
+
+
+def uppercase(string):
+    """Print a string in uppercase."""
+    for c in string:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-        print(c, end="")
-    print()
-
-
-if __name__ == "__main__":
-    uppercase("best")
-    uppercase("Best School 98 Battery street")
+        print("{}".format(c), end="")
+    print("")
