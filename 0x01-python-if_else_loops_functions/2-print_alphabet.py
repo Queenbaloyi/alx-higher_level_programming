b9ae0c28060717a7d53698977151f26ebf3b940b
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-09 20:09:24 -0700

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-def get_alphabet_letters():
-    """Returns a list of all the lowercase letters."""
-    alphabet_letters = []
-    for letter in range(97, 123):
-        alphabet_letters.append(chr(letter))
-    return alphabet_letters
+# 2-print_alphabet.py
 
-def print_alphabet():
-    """Prints the lowercase letters of the alphabet, not followed by a new line."""
-    alphabet_letters = get_alphabet_letters()
-    for letter in alphabet_letters:
-        print(letter, end="")
-
-if __name__ == "__main__":
-    print_alphabet()
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
