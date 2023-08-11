fb2a4f548ba3102d794636ffb5b5b87c2c0397ec
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 17:05:06 -0700

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,35 +1,8 @@
-#include "lists.h"
+#!/usr/bin/python3
+# 100-print_tebahpla.py
 
-/**
- * insert_list_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- *
- * Return: If the function fails - NULL.
- * Otherwise - a pointer to the new list_node.
- */
-listint_t *insert_list_node(listint_t **head, int number)
-{
-	listint_t *list_node = *head, *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-
-	if (list_node == NULL || list_node->n >= number)
-	{
-		new->next = list_node;
-		*head = new;
-		return (new);
-	}
-
-	while (list_node && list_node->next && list_node->next->n < number)
-		list_node = list_node->next;
-
-	new->next = list_node->next;
-	list_node->next = new;
-
-	return (new);
-}
-
+""""Print the alphabet in reverse order alternating upper- and lower-case."""
+current_letter = 0
+for letter in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(letter - current_letter)), end="")
+    current_letter = 32 if current_letter == 0 else 0
