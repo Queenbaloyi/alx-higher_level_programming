f31256f63314703df12ee0bc0a0ea90a37c72414
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 17:18:59 -0700

    update

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 # 102-magic_calculation.py
 
-def magic_calculation(first_number, second_number, third_number):
-    """Calculates a magic number based on the given three numbers."""
-    if first_number < second_number:
-        return third_number
-    elif third_number > second_number:
-        return first_number + second_number
-    else:
-        return first_number * second_number - third_number
 
+def magic_calculation(first_number, second_number, third_number):
+    """Match bytecode provided by Holberton School."""
+    if first_number < second_number:
+        return (third_number)
+    if third_number > second_number:
+        return (first_number + second_number)
+    return (first_number*second_number - third_number)
