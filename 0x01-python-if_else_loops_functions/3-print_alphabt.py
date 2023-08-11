d9153248682aa74085d2b4fafcd2b1525b55583c
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 17:32:39 -0700

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet_letter in range(97, 123):
-    if chr(alphabet_letter) is not 'q' and chr(alphabet_letter) is not 'e':
-        print("{}".format(chr(alphabet_letter)), end="")
+for lowercase_letter in range(97, 123):
+    if chr(lowercase_letter) is not 'q' and chr(lowercase_letter) is not 'e':
+        print("{}".format(chr(lowercase_letter)), end="")
