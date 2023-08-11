3d3f52ccbaf7dd4373bd373d1891a6b74874f148
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 17:44:40 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-my_string = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-my_string = my_string[39:67] + my_string[107:112] + my_string[:6]
-print(my_string)
+str = python_features[39:67] + python_features[107:112] + python_features[:6]
+print(python_features)
