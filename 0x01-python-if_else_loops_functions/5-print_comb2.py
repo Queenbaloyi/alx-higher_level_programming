d6dccca4e814de7b635581dd0b0f2afe2cf79f82
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-10 17:28:57 -0700

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
-
-def print_numbers(start_number, end_number):
-  """Prints a list of numbers from start_number to end_number, formatted as two-digit strings.
-
-  Args:
-    start_number: The first number to print.
-    end_number: The last number to print.
-  """
-
-  for number in range(start_number, end_number + 1):
-    if number == end_number:
-      print(number)
+for current_number in range(0, 100):
+    if current_number == 99:
+        print("{}".format(current_number))
     else:
-      print("{:02}".format(number), end=", ")
-
-if __name__ == "__main__":
-  print_numbers(0, 100)
+        print("{:02}".format(current_number), end=", ")
