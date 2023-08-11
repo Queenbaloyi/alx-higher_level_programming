02e3d28847dcaff6adc3edcc9c816cbe021eb035
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-11 16:19:35 -0700

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-def add(x, y):
-    """This function adds two numbers."""
-    return x + y
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
 
+    Args:
+        a: first integer
+        b: second integer
 
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from my_add_function import add
-
-    first_number = 1
-    second_number = 2
-    print("{} + {} = {}".format(first_number, second_number, add(first_number, second_number)))
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
