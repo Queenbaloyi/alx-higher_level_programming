71b731bbb1ce78fc8d0c30197821d07a208c1041
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-12 13:29:04 -0700

    update

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,5 +3,11 @@
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
-    
-    print(100)
+
+    # Get a reference to the original variable a
+    a_global = globals()["a"]
+
+    # Change the value of a_global to 100
+    setattr(globals(), "a", 100)
+
+    print(a)
