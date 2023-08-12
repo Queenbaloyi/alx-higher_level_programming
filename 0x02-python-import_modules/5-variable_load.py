c545f7923c60d220d30fa08760430ad3ebf404e2
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-12 13:11:12 -0700

    update

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a=100
-
-    print(a)
+a = 100
+"""Simple variable
+"""
