869dd645363415e5d323ff3aa6b019cbe839a8c1
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-12 13:16:01 -0700

    update

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
 
-    # Change the value of a to 100
     a = 100
 
-    print(a)
+    print(100)
