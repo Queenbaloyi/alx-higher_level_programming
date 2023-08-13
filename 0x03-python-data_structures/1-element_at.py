3913e88f5bf11f2c777d05dcdae76d0b38705277
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-13 14:01:18 -0700

    update

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def retrieve_element(my_sequence, index):
+    """Retrieve an element from a sequence."""
+    if index < 0 or index >= len(my_sequence):
+        return None
+    return my_sequence[index]
