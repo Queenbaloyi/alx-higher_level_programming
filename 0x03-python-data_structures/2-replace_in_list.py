42b2b0f65e17fafdcf94a12e12114f5610d35c2d
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-13 14:34:46 -0700

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 2-replace_in_list.py
 
-def replace_element_in_list(my_sequence, index, element):
+def replace_element_in_sequence(my_sequence, index, element):
     """Replace an element of a sequence at a specific position."""
     if index < 0 or index >= len(my_sequence):
         return my_sequence
