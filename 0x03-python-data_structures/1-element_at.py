bf2bcf8af68118c70b52b6c38ca1fa9cd6ac6cde
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-13 14:14:19 -0700

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,3 @@ def retrieve_element(my_sequence, index):
     if index < 0 or index >= len(my_sequence):
         return None
     return my_sequence[index]
-
-if __name__ == "__main__":
-    my_sequence = [1, 2, 3, 4, 5]
-    index = 3
-    print(retrieve_element(my_sequence, index))
