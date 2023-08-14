8ed4e5e3bd20cc4e7464bd59e8164b05358e277f
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-14 14:47:02 -0700

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-
-    # Check if the input is a list of lists
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a list of lists")
-
-    # Print the matrix
-    for row in matrix:
+def print_matrix_integer(my_matrix=[[]]):
+    for row in my_matrix:
         for col in row:
-            print(f"{col:d}", end=" ")
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
