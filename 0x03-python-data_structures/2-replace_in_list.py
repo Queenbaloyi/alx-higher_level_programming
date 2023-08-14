a291809c0cdc8f3de7262f989514b351c8f5b8b9
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-14 16:15:07 -0700

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 # 2-replace_in_list.py
 
-def replace_element_in_sequence(my_sequence, index, element):
-    """Replace an element of a sequence at a specific position."""
-    if index < 0 or index >= len(my_sequence):
-        return my_sequence
-    my_sequence_copy = my_sequence[:]
-    my_sequence_copy[index] = element
-    return my_sequence_copy
+def replace_in_list(my_list, idx, element):
+    """Replace an element of a list at a specific position."""
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    my_list[idx] = element
+    return my_list
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 2
+    element = 10
+    print(f"The original list is: {my_list}")
+    print(f"The modified list is: {replace_in_list(my_list, idx, element)}")
