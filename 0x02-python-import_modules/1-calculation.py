41c0aa6462f5c34673b6a9e1fbb29b53e72ca105
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-15 13:16:04 -0700

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ import calculator_1
 a = 10
 b = 5
 
-print(f"{a} + {b} = {calculator_1.add(a, b)}")
-print(f"{a} - {b} = {calculator_1.sub(a, b)}")
-print(f"{a} * {b} = {calculator_1.mul(a, b)}")
-print(f"{a} / {b} = {calculator_1.div(a, b)}")
+print("Addition: {} + {} = {}".format(a, b, calculator_1.add(a, b)))
+print("Subtraction: {} - {} = {}".format(a, b, calculator_1.subtract(a, b)))
+print("Multiplication: {} * {} = {}".format(a, b, calculator_1.multiply(a, b)))
+print("Division: {} / {} = {}".format(a, b, calculator_1.divide(a, b)))
