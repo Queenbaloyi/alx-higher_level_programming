f7ea3bff1928efcd093c6818ec235de5f52478f1
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-15 13:46:47 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 
+def print_arguments():
+  """Prints the number of and the list of its arguments."""
+  # Get the number of arguments
+  num_args = len(argv)
+
+  # If no arguments were passed, print a message and exit
+  if num_args == 0:
+    print("No arguments were passed.")
+    return
+
+  # Print the number of arguments
+  print("Number of arguments:", num_args)
+
+  # Print the list of arguments
+  print("Arguments:")
+  for i in range(1, num_args + 1):
+    print(i, ":", argv[i - 1])
+
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
-    import sys
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    x = int(sys.argv[1])
-    y = int(sys.argv[3])
-    print("{} {} {} = {}".format(x, operator, y, ops[sys.argv[2]](x, y)))
+  # This line will be executed only if the program is run directly,
+  # not if it is imported as a module
+  print_arguments()
