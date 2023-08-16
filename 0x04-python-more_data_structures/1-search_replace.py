f9ab35235d8d6129731ed558cfd8aca271a0f1bc
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-16 14:34:18 -0700

    update

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    return new_list
+
+if __name__ == "__main__":
+    my_list = ["apple", "banana", "cherry"]
+    search = "apple"
+    replace = "orange"
+    new_list = search_replace(my_list, search, replace)
+    print(new_list)
