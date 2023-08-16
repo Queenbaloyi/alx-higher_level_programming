5ec6d76f230f3ac11cfb91069068d6b220cc5a2c
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-16 15:39:08 -0700

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+  """Replaces or adds key/value in a dictionary.
+
+  Args:
+    a_dictionary: a dictionary
+    key: a string
+    value: any type
+
+  If a key exists in the dictionary, the value will be replaced
+  If a key doesn’t exist in the dictionary, it will be created
+
+  You are not allowed to import any module
+  """
 
   if key in a_dictionary:
     a_dictionary[key] = value
