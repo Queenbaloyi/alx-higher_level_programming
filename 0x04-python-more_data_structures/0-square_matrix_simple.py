e793196f0797368cee89737ca079c29b3d9480f0
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-16 14:23:17 -0700

    update

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+  """Computes the square value of all integers of a matrix.
+
+  Args:
+    matrix: A 2 dimensional array.
+
+  Returns:
+    A new matrix:
+      Same size as matrix
+      Each value should be the square of the value of the input
+      Initial matrix should not be modified
+  """
+
+  new_matrix = []
+  for row in matrix:
+    new_row = []
+    for value in row:
+      new_row.append(value ** 2)
+    new_matrix.append(new_row)
+
+  return new_matrix
