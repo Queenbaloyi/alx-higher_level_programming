a931e0a2dfe737959b81fa7f7eeebaa22fb75fd6
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-16 15:16:09 -0700

    update

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -7,9 +7,3 @@ def number_keys(a_dictionary):
 
   Returns:
     The number of keys in the dictionary
-
-  You are not allowed to import any module
-  """
-
-  num_keys = len(a_dictionary)
-  return num_keys
