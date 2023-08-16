022186126e5c34fe6fac73f077aa7c8a6fce068d
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-16 15:08:56 -0700

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
+def square_matrix_simple(matrix):
   """Computes the square value of all integers of a matrix.
 
   Args:
-    matrix: A 2 dimensional array.
+    matrix: a 2 dimensional array
 
   Returns:
-    A new matrix:
+    a new matrix:
       Same size as matrix
       Each value should be the square of the value of the input
-      Initial matrix should not be modified
+    Initial matrix should not be modified
+
+  You are not allowed to import any module
+  You are allowed to use regular loops, map, etc.
   """
 
   new_matrix = []
   for row in matrix:
     new_row = []
-    for value in row:
-      new_row.append(value ** 2)
+    for item in row:
+      new_row.append(item ** 2)
     new_matrix.append(new_row)
-
   return new_matrix
