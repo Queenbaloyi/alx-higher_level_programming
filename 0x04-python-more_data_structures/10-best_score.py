1a668d8729f5c9bbfa6bd06f1b6b28f6f2080687
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-16 16:01:46 -0700

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-  """Returns a key with the biggest integer value.
-
-  Args:
-    a_dictionary: a dictionary
-
-  You can assume that all values are only integers
-
-  If no score found, return None
-  You can assume all students have a different score
-
-  You are not allowed to import any module
-  """
 
   if a_dictionary is None:
     return None
