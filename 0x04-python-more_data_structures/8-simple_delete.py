9b1caef370a355f893f1e9b07100b9c5d15fd6d0
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-16 15:31:45 -0700

    update

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+  """Replaces or adds key/value in a dictionary.
+
+  Args:
+    a_dictionary: a dictionary
+    key: a string
+    value: any type
+
+    if key in a_dictionary:
+    a_dictionary[key] = value
+  else:
+    a_dictionary[key] = value
+  return a_dictionary
