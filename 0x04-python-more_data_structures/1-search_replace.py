19f0ab2ecdb92e3adecd0e90cba8c671c08ce847
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-16 14:35:22 -0700

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,7 +7,6 @@ def search_replace(my_list, search, replace):
         else:
             new_list.append(item)
     return new_list
-
 if __name__ == "__main__":
     my_list = ["apple", "banana", "cherry"]
     search = "apple"
