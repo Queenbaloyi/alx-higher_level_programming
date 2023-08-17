4920ea7e832f15ea4b05448c55407352fb58f14f
Queenbaloyi <queenbaloyi16@gmail.com>
2023-08-17 15:58:57 -0700

    update

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+size_t print_dlistint(const dlistint_t *h)
+{
+  const dlistint_t *current;
+  size_t count;
+
+  count = 0;
+  for (current = h; current != NULL; current = current->next)
+  {
+    printf("%d\n", current->n);
+    count++;
+  }
+
+  return count;
+}
