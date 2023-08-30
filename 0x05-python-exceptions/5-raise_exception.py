d109d915ead3532c21ba505dafb6459ed6320363
queenbaloyi <queenbaloyi16@gmail.com>
2023-08-30 13:43:00 -0700

    update

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -2,4 +2,6 @@
  """Raise a TypeError exception."""
 
 def raise_exception():
-    raise TypeError
+    raise TypeError("This is a custom TypeError exception.")
+if __name__ == "__main__":
+    raise_exception()
